007dcdac9066b7951f12317fe60afc9df74ba1a9
bendo63 <benbrochet@orange.fr>
2024-01-23 23:14:15 +0100

    ⚡️ add opti

--- app/api_expose.py
+++ app/api_expose.py
@@ -3,12 +3,15 @@ import time
 from dotenv import load_dotenv
 from prometheus_client import start_http_server, Gauge, Info
 from api_request import get_request, post_with_headers_request
+import concurrent.futures
 
 existing_metrics = {}
+
 
 def start_prometheus():
     load_dotenv()
     start_http_server(int(os.getenv("PORT_HTTP")))
+
 
 def get_or_create_gauge(name, description, labelnames=[]):
     if name not in existing_metrics:
@@ -18,6 +21,7 @@ def get_or_create_gauge(name, description, labelnames=[]):
         gauge = existing_metrics[name]
     return gauge
 
+
 def get_or_create_info(name, description):
     if name not in existing_metrics:
         info_metric = Info(name, description)
@@ -26,12 +30,31 @@ def get_or_create_info(name, description):
         info_metric = existing_metrics[name]
     return info_metric
 
+
 def time_script(start_time):
-    time_script = get_or_create_gauge('freebox_time_script', 'Metrics generation time')
+    time_script = get_or_create_gauge(
+        'freebox_time_script', 'Metrics generation time')
     end_time = time.time()
     duration = end_time - start_time
     formatted_duration = "{:.2f}".format(duration)
     time_script.set(formatted_duration)
+
+
+def optimize_code(headers):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(system_metrics, headers),
+            executor.submit(lan_browser_pub_metrics, headers),
+            executor.submit(lan_config, headers),
+            executor.submit(port_forwarding, headers),
+            executor.submit(port_incoming, headers),
+            executor.submit(vpn_connection, headers),
+            executor.submit(rrd_net, headers),
+            executor.submit(rrd_switch, headers),
+            executor.submit(storage_disk, headers)
+        ]
+        concurrent.futures.wait(futures)
+
 
 def system_metrics(headers):
     system_request = get_request("system/", headers=headers)
@@ -41,13 +64,16 @@ def system_metrics(headers):
     temp_cpu_cp_master = system_request['result']['sensors'][3]['value']
     temp_cpu_ap = system_request['result']['sensors'][4]['value']
     temp_cpu_cp_slave = system_request['result']['sensors'][5]['value']
-    temp = get_or_create_gauge('freebox_temperature', 'Température', labelnames=['temp_t1', 'temp_t2', 'temp_t3', 'temp_cpu_cp_master', 'temp_cpu_ap', 'temp_cpu_cp_slave'])
-    temp.labels(temp_t1=temp_t1, temp_t2=temp_t2,temp_t3=temp_t3,temp_cpu_cp_master=temp_cpu_cp_master,temp_cpu_ap=temp_cpu_ap,temp_cpu_cp_slave=temp_cpu_cp_slave)
+    temp = get_or_create_gauge('freebox_temperature', 'Température', labelnames=[
+                               'temp_t1', 'temp_t2', 'temp_t3', 'temp_cpu_cp_master', 'temp_cpu_ap', 'temp_cpu_cp_slave'])
+    temp.labels(temp_t1=temp_t1, temp_t2=temp_t2, temp_t3=temp_t3, temp_cpu_cp_master=temp_cpu_cp_master,
+                temp_cpu_ap=temp_cpu_ap, temp_cpu_cp_slave=temp_cpu_cp_slave)
 
     fan0_speed = system_request['result']['fans'][1]['value']
     fan1_speed = system_request['result']['fans'][0]['value']
-    fans = get_or_create_gauge('freebox_fan_speed', 'Ventilateur', labelnames=['fan0_speed', 'fan1_speed'])
-    fans.labels(fan0_speed=fan0_speed,fan1_speed=fan1_speed)
+    fans = get_or_create_gauge('freebox_fan_speed', 'Ventilateur', labelnames=[
+                               'fan0_speed', 'fan1_speed'])
+    fans.labels(fan0_speed=fan0_speed, fan1_speed=fan1_speed)
 
     firmware_version = get_or_create_info(
         'freebox_firmware_version', 'Firmware version')
@@ -64,17 +90,21 @@ def lan_browser_pub_metrics(headers):
         mac_address = item['l2ident']['id']
         active = item['active']
         vendor_name = item['vendor_name']
-        host_type = item['host_type']   
+        host_type = item['host_type']
         last_time_reachable = item['last_time_reachable']
         ip = item['l3connectivities'][0]['addr']
         reachable = item.get('reachable', False)
         last_activity = item['last_activity']
         default_name = item['default_name']
-        connectivity_type = item.get('access_point', {}).get('connectivity_type', '')
+        connectivity_type = item.get(
+            'access_point', {}).get('connectivity_type', '')
         first_activity = item['first_activity']
         primary_name = item['primary_name']
-        lan_browser_pub = get_or_create_gauge('freebox_lan_browser_pub', 'Lan browser pub', ['mac_address', 'active', 'vendor_name', 'host_type', 'last_time_reachable', 'ip', 'last_activity', 'connectivity_type', 'default_name', 'first_activity', 'primary_name'])
-        lan_browser_pub.labels(mac_address=mac_address,active=active,vendor_name=vendor_name,host_type=host_type,last_time_reachable=last_time_reachable,ip=ip,last_activity=last_activity,connectivity_type=connectivity_type,default_name=default_name,first_activity=first_activity,primary_name=primary_name).set(1 if reachable else 0)
+        lan_browser_pub = get_or_create_gauge('freebox_lan_browser_pub', 'Lan browser pub', [
+                                              'mac_address', 'active', 'vendor_name', 'host_type', 'last_time_reachable', 'ip', 'last_activity', 'connectivity_type', 'default_name', 'first_activity', 'primary_name'])
+        lan_browser_pub.labels(mac_address=mac_address, active=active, vendor_name=vendor_name, host_type=host_type, last_time_reachable=last_time_reachable, ip=ip,
+                               last_activity=last_activity, connectivity_type=connectivity_type, default_name=default_name, first_activity=first_activity, primary_name=primary_name).set(1 if reachable else 0)
+
 
 def lan_config(headers):
     lan_config_request = get_request("lan/config/", headers=headers)
@@ -84,8 +114,11 @@ def lan_config(headers):
     mode = lan_config_request['result']['mode']
     name_netbios = lan_config_request['result']['name_netbios']
     ip = lan_config_request['result']['ip']
-    lan_config = get_or_create_gauge('freebox_lan_config', 'Lan config', ['name_dns', 'name_mdns', 'name', 'mode', 'name_netbios', 'ip'])
-    lan_config.labels(name_dns=name_dns, name_mdns=name_mdns, name=name, mode=mode, name_netbios=name_netbios, ip=ip)
+    lan_config = get_or_create_gauge('freebox_lan_config', 'Lan config', [
+                                     'name_dns', 'name_mdns', 'name', 'mode', 'name_netbios', 'ip'])
+    lan_config.labels(name_dns=name_dns, name_mdns=name_mdns,
+                      name=name, mode=mode, name_netbios=name_netbios, ip=ip)
+
 
 def port_forwarding(headers):
     port_forwarding_request = get_request("fw/redir/", headers=headers)
@@ -102,10 +135,11 @@ def port_forwarding(headers):
             host = item.get('host', '')
             src_ip = item.get('src_ip', '')
             comment = item.get('comment', '')
-            port_forwarding = get_or_create_gauge('freebox_port_forwarding', 'Port forwarding', ['id', 'enabled', 'ip_proto', 'wan_port_start', 'wan_port_end', 'lan_ip', 'lan_port', 'hostname', 'host', 'src_ip', 'comment'])
-            port_forwarding.labels(id=id, enabled=enabled, ip_proto=ip_proto, wan_port_start=wan_port_start, wan_port_end=wan_port_end, lan_ip=lan_ip, lan_port=lan_port, hostname=hostname, host=host, src_ip=src_ip, comment=comment).set(1 if enabled else 0)
-    else:
-        print("No port forwarding rules found.")
+            port_forwarding = get_or_create_gauge('freebox_port_forwarding', 'Port forwarding', [
+                                                  'id', 'enabled', 'ip_proto', 'wan_port_start', 'wan_port_end', 'lan_ip', 'lan_port', 'hostname', 'host', 'src_ip', 'comment'])
+            port_forwarding.labels(id=id, enabled=enabled, ip_proto=ip_proto, wan_port_start=wan_port_start, wan_port_end=wan_port_end,
+                                   lan_ip=lan_ip, lan_port=lan_port, hostname=hostname, host=host, src_ip=src_ip, comment=comment).set(1 if enabled else 0)
+
 
 def port_incoming(headers):
     port_incoming_request = get_request("fw/incoming/", headers=headers)
@@ -119,8 +153,11 @@ def port_incoming(headers):
         in_port = item['in_port']
         readonly = item['readonly']
         netns = item['netns']
-        port_incoming = get_or_create_gauge('freebox_port_incoming', 'Port incoming', ['id', 'enabled', 'type', 'active', 'max_port', 'min_port', 'in_port', 'readonly', 'netns'])
-        port_incoming.labels(id=id, type=type, enabled=enabled, active=active, max_port=max_port, min_port=min_port, in_port=in_port, readonly=readonly, netns=netns).set(1 if enabled else 0)
+        port_incoming = get_or_create_gauge('freebox_port_incoming', 'Port incoming', [
+                                            'id', 'enabled', 'type', 'active', 'max_port', 'min_port', 'in_port', 'readonly', 'netns'])
+        port_incoming.labels(id=id, type=type, enabled=enabled, active=active, max_port=max_port,
+                             min_port=min_port, in_port=in_port, readonly=readonly, netns=netns).set(1 if enabled else 0)
+
 
 def vpn_connection(headers):
     vpn_connection_request = get_request("vpn/connection/", headers=headers)
@@ -134,18 +171,20 @@ def vpn_connection(headers):
             src_ip = item['src_ip']
             auth_time = item['auth_time']
             local_ip = item['local_ip']
-        vpn_connection = get_or_create_gauge('freebox_vpn_connection', 'User connect to VPN', ['rx_bytes', 'tx_bytes', 'user', 'vpn', 'src_port', 'src_ip', 'auth_time', 'local_ip'])                     
-        vpn_connection.labels(rx_bytes=rx_bytes, tx_bytes=tx_bytes, user=user, vpn=vpn, src_port=src_port, src_ip=src_ip, auth_time=auth_time, local_ip=local_ip)
-    else:
-        print("No user connect to VPN.")
+        vpn_connection = get_or_create_gauge('freebox_vpn_connection', 'User connect to VPN', [
+                                             'rx_bytes', 'tx_bytes', 'user', 'vpn', 'src_port', 'src_ip', 'auth_time', 'local_ip'])
+        vpn_connection.labels(rx_bytes=rx_bytes, tx_bytes=tx_bytes, user=user, vpn=vpn,
+                              src_port=src_port, src_ip=src_ip, auth_time=auth_time, local_ip=local_ip)
+
 
 def rrd_net(headers):
     session_data = {
         "db": "net",
-        "fields": [ "bw_up", "bw_down", "rate_up", "rate_down", "vpn_rate_up", "vpn_rate_down" ],
+        "fields": ["bw_up", "bw_down", "rate_up", "rate_down", "vpn_rate_up", "vpn_rate_down"],
         "precision": 10
     }
-    rrd_net_request = post_with_headers_request("rrd/", session_data, headers=headers)
+    rrd_net_request = post_with_headers_request(
+        "rrd/", session_data, headers=headers)
     data0_request = rrd_net_request['result']['data'][0]
     bw_up = data0_request.get('bw_up', '')
     bw_down = data0_request.get('bw_down', '')
@@ -153,16 +192,20 @@ def rrd_net(headers):
     rate_down = data0_request.get('rate_down', '')
     vpn_rate_up = data0_request.get('vpn_rate_up', '')
     vpn_rate_down = data0_request.get('vpn_rate_down', '')
-    rrd_net = get_or_create_gauge('freebox_net_states', 'Net stats', ['bw_up', 'bw_down', 'rate_up', 'rate_down', 'vpn_rate_up', 'vpn_rate_down'])
-    rrd_net.labels(bw_up=bw_up, bw_down=bw_down, rate_up=rate_up, rate_down=rate_down, vpn_rate_up=vpn_rate_up, vpn_rate_down=vpn_rate_down)
+    rrd_net = get_or_create_gauge('freebox_net_states', 'Net stats', [
+                                  'bw_up', 'bw_down', 'rate_up', 'rate_down', 'vpn_rate_up', 'vpn_rate_down'])
+    rrd_net.labels(bw_up=bw_up, bw_down=bw_down, rate_up=rate_up,
+                   rate_down=rate_down, vpn_rate_up=vpn_rate_up, vpn_rate_down=vpn_rate_down)
+
 
 def rrd_switch(headers):
     session_data = {
         "db": "switch",
-        "fields": [ "rx_1", "tx_1", "rx_2", "tx_2", "rx_3", "tx_3", "rx_4", "tx_4" ],
+        "fields": ["rx_1", "tx_1", "rx_2", "tx_2", "rx_3", "tx_3", "rx_4", "tx_4"],
         "precision": 10
     }
-    rrd_switch_request = post_with_headers_request("rrd/", session_data, headers=headers)
+    rrd_switch_request = post_with_headers_request(
+        "rrd/", session_data, headers=headers)
     data0_request = rrd_switch_request['result']['data'][0]
     rx_1 = data0_request.get('rx_1', '')
     tx_1 = data0_request.get('tx_1', '')
@@ -172,8 +215,11 @@ def rrd_switch(headers):
     tx_3 = data0_request.get('tx_3', '')
     rx_4 = data0_request.get('rx_4', '')
     tx_4 = data0_request.get('tx_4', '')
-    rrd_switch = get_or_create_gauge('freebox_switch_states', 'Switch stats', ['rx_1', 'tx_1', 'rx_2', 'tx_2', 'rx_3', 'tx_3', 'rx_4', 'tx_4'])
-    rrd_switch.labels(rx_1=rx_1,tx_1=tx_1,rx_2=rx_2,tx_2=tx_2,rx_3=rx_3,tx_3=tx_3,rx_4=rx_4,tx_4=tx_4)
+    rrd_switch = get_or_create_gauge('freebox_switch_states', 'Switch stats', [
+                                     'rx_1', 'tx_1', 'rx_2', 'tx_2', 'rx_3', 'tx_3', 'rx_4', 'tx_4'])
+    rrd_switch.labels(rx_1=rx_1, tx_1=tx_1, rx_2=rx_2, tx_2=tx_2,
+                      rx_3=rx_3, tx_3=tx_3, rx_4=rx_4, tx_4=tx_4)
+
 
 def storage_disk(headers):
     storage_disk_request = get_request("storage/disk/", headers=headers)
@@ -204,7 +250,7 @@ def storage_disk(headers):
             free_bytes = item['partitions'][0]['free_bytes']
             used_bytes = item['partitions'][0]['used_bytes']
             path = item['partitions'][0]['path']
-        storage_disk = get_or_create_gauge('freebox_storage_disk', 'Disk status', ['idle_duration', 'read_error_requests', 'read_requests', 'spinning', 'table_type', 'firmware', 'type', 'idle', 'connector', 'id', 'write_error_requests', 'state', 'write_requests', 'total_bytes', 'model', 'active_duration', 'temp', 'serial', 'fstype', 'label', 'internal', 'fsck_result', 'free_bytes', 'used_bytes', 'path'])                     
-        storage_disk.labels(idle_duration=idle_duration,read_error_requests=read_error_requests,read_requests=read_requests,spinning=spinning,table_type=table_type,firmware=firmware,type=type,idle=idle,connector=connector,id=id,write_error_requests=write_error_requests,state=state,write_requests=write_requests,total_bytes=total_bytes,model=model,active_duration=active_duration,temp=temp,serial=serial,fstype=fstype,label=label,internal=internal,fsck_result=fsck_result,free_bytes=free_bytes,used_bytes=used_bytes,path=path)
-    else:
-        print("No disk connected.")
+        storage_disk = get_or_create_gauge('freebox_storage_disk', 'Disk status', ['idle_duration', 'read_error_requests', 'read_requests', 'spinning', 'table_type', 'firmware', 'type', 'idle', 'connector',
+                                           'id', 'write_error_requests', 'state', 'write_requests', 'total_bytes', 'model', 'active_duration', 'temp', 'serial', 'fstype', 'label', 'internal', 'fsck_result', 'free_bytes', 'used_bytes', 'path'])
+        storage_disk.labels(idle_duration=idle_duration, read_error_requests=read_error_requests, read_requests=read_requests, spinning=spinning, table_type=table_type, firmware=firmware, type=type, idle=idle, connector=connector, id=id, write_error_requests=write_error_requests,
+                            state=state, write_requests=write_requests, total_bytes=total_bytes, model=model, active_duration=active_duration, temp=temp, serial=serial, fstype=fstype, label=label, internal=internal, fsck_result=fsck_result, free_bytes=free_bytes, used_bytes=used_bytes, path=path)
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import time
 import os
 from dotenv import load_dotenv
 from api_session import open_session, close_session
-from api_expose import start_prometheus, system_metrics, lan_browser_pub_metrics, lan_config, port_forwarding, port_incoming, time_script, vpn_connection, rrd_net, rrd_switch, storage_disk
+from api_expose import start_prometheus, time_script, optimize_code
 
 load_dotenv()
 start_prometheus()
@@ -11,15 +11,7 @@ headers = open_session()
 try:
     while True:
         start_time = time.time()
-        system_metrics(headers)
-        lan_browser_pub_metrics(headers)
-        lan_config(headers)
-        port_forwarding(headers)
-        port_incoming(headers)
-        vpn_connection(headers)
-        rrd_net(headers)
-        rrd_switch(headers)
-        storage_disk(headers)
+        optimize_code(headers)
         time_script(start_time)
         time.sleep(int(os.getenv("SCRAPE_INTERVAL")))
 
