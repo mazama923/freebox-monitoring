8713653ee9f07036756451b505581a165dc903a9
Benjamin <42283374+mazama923@users.noreply.github.com>
2024-01-25 00:07:42 +0100

    removal of multi request for stability issues (#5)
    Co-authored-by: bendo63 <benbrochet@orange.fr>

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import time
 import os
 from dotenv import load_dotenv
 from api_session import open_session, close_session
-from api_expose import start_prometheus, time_script, optimize_code
+from api_expose import start_prometheus, time_script, system_metrics, lan_browser_pub_metrics, lan_config, port_forwarding, port_incoming, vpn_connection, rrd_net, rrd_switch, storage_disk
 
 load_dotenv()
 start_prometheus()
@@ -11,8 +11,17 @@ headers = open_session()
 try:
     while True:
         start_time = time.time()
-        optimize_code(headers)
+        system_metrics(headers)
+        lan_browser_pub_metrics(headers)
+        lan_config(headers)
+        port_forwarding(headers)
+        port_incoming(headers)
+        vpn_connection(headers)
+        rrd_net(headers)
+        rrd_switch(headers)
+        storage_disk(headers)
         time_script(start_time)
+        print("------")
         time.sleep(int(os.getenv("SCRAPE_INTERVAL")))
 
 finally:
